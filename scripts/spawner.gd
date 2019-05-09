79f0887a1a990985d5fab211979c143499784f19
Emanuel Silva <emanueljulianoms@gmail.com>
2019-05-08 21:33:06 -0300

    Added list of spawners

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -8,8 +8,8 @@ var pre_larissa = preload("res://scenes/larissa.tscn")
 var pre_margaret = preload("res://scenes/margaret.tscn")
 var pre_bebel = preload("res://scenes/bebel.tscn")
 var pre_barbara = preload("res://scenes/barbara.tscn")
-var lista_inimigos = [pre_barbara]
-#_antony, pre_monica, pre_larissa, pre_margaret, pre_bebel]
+var lista_inimigos = [pre_barbara]#, pre_antony, pre_monica, pre_larissa, pre_margaret, pre_bebel]
+# pre_barbara, pre_antony, pre_monica, pre_larissa, pre_margaret, pre_bebel
 var spawn_pos = Vector2()
 var rand_n
 
